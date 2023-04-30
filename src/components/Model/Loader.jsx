bd428f718d0cfb2e9fabf7dcf6b7e4e8efa4ac7b
hardikverma22 <hardikverma22@rediffmail.com>
2023-04-30 21:34:44 +0530

    changing model loader animation using framer motion

--- src/components/Model/Loader.jsx
+++ src/components/Model/Loader.jsx
@@ -1,7 +1,33 @@
 import { Html, useProgress } from "@react-three/drei";
+import { motion } from "framer-motion";
 
 const CanvasLoader = () => {
   const { progress } = useProgress();
+
+  const containerVariants = {
+    start: {
+      opacity: 0,
+    },
+    end: {
+      opacity: 1,
+    },
+  };
+
+  const circleVariants = {
+    start: {
+      y: "0%",
+    },
+    end: {
+      y: "100%",
+    },
+  };
+
+  const circleTransition = {
+    duration: 1,
+    repeat: Infinity,
+    ease: "easeInOut",
+  };
+
   return (
     <Html
       as="div"
@@ -13,17 +39,44 @@ const CanvasLoader = () => {
         flexDirection: "column",
       }}
     >
-      <span className="canvas-loader"></span>
-      <p
-        style={{
-          fontSize: 14,
-          color: "#F1F1F1",
-          fontWeight: 800,
-          marginTop: 40,
-        }}
-      >
-        {progress.toFixed(2)}%
-      </p>
+      <div className="flex flex-col gap-2 justify-center items-center w-full h-screen">
+        <p className="text-black text-lg font-bold">{progress.toFixed(2)}%</p>
+        <motion.div className="flex items-center">
+          <motion.div
+            className="w-4 h-4 rounded-full mr-2 bg-primary"
+            animate={{ y: 10 }}
+            initial={{ y: 0 }}
+            transition={{
+              duration: 0.3,
+              repeat: Infinity,
+              repeatType: "reverse",
+              delay: 0,
+            }}
+          />
+          <motion.div
+            className="w-4 h-4 rounded-full mr-2 bg-secondary"
+            animate={{ y: 10 }}
+            initial={{ y: 0 }}
+            transition={{
+              duration: 0.3,
+              repeat: Infinity,
+              repeatType: "reverse",
+              delay: 0.2,
+            }}
+          />
+          <motion.div
+            className="w-4 h-4 rounded-full bg-tertiary"
+            animate={{ y: 10 }}
+            initial={{ y: 0 }}
+            transition={{
+              duration: 0.3,
+              repeat: Infinity,
+              repeatType: "reverse",
+              delay: 0.4,
+            }}
+          />
+        </motion.div>
+      </div>
     </Html>
   );
 };
